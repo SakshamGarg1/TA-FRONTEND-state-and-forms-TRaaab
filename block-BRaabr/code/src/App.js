a588b5d2fa4542aee24c9787ecb725e3553b0a36
SakshamGarg1 <86765712+SakshamGarg1@users.noreply.github.com>
2022-06-27 21:34:12 +0530

    Add files via upload

---
+++ block-BRaabr/code/src/App.js
@@ -0,0 +1,13 @@
+import Validation from "./validation";
+ import MultipleInput from "./MultipleInputs";
+
+ function App() {
+   return (
+     <>
+       <MultipleInput />
+       <Validation />
+     </>
+   );
+ }
+
+ export default App;
